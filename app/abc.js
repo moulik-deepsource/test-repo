4b5c639db5e4773a058273a8e4505bde5916dae2
Moulik Aggarwal <qwertymoulik@gmail.com>
2020-12-24 14:36:00 +0530

    Add more tests
    Signed-off-by: Moulik Aggarwal <qwertymoulik@gmail.com>

--- app/abc.js
+++ app/abc.js
@@ -28,3 +28,29 @@ function checkIsEqual(a, b){
 }
 
 checkIsEqual(10, "10")
+
+!!!foo
+
+var foo = /bar   {3}baz/;
+
+if (a) { if (b) console.log(1); else console.log(2) } else console.log(3)
+
+do {foo();} while (bar)
+
+do{[1, 2, 3].map(bar);} while (bar)
+
+if (foo) bar()
+
+if (foo) { bar() } else baz()
+
+let a = {
+  true: ''
+}
+
+a['true']
+
+var b = !!foo;
+var b = ~foo.indexOf(".");
+var n = +foo;
+var n = 1 * foo;
+
